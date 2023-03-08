43394ff5801cde1bac63ae091f99c0e7eddd1f3b
leejisu <ljsgoo0617@gmail.com>
2023-03-09 00:49:55 +0900

    배열 생성 및 입력 받기

--- C_CodingTest/11-1-1.c
+++ C_CodingTest/11-1-1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
 int main(void) {
+	int arr[5] = { 0 };
+	int arrSize = sizeof(arr) / sizeof(arr[0]);
+	int max = 0, min = 0, total = 0;
 
+	for (int i = 0; i < arrSize; i++) {
+		scanf_s("%d", &arr[i]);
+	}
 }
