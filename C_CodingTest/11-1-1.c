54589011559613e0d2d837b4430b0802f64acce6
leejisu <ljsgoo0617@gmail.com>
2023-03-11 23:22:04 +0900

    int타입의 가장 작은 수와 가장 큰 수를 이용해 최댓값, 최솟값 오류 해결 (11-1-1 완성)

--- C_CodingTest/11-1-1.c
+++ C_CodingTest/11-1-1.c
@@ -3,15 +3,14 @@
 int main(void) {
 	int arr[5] = { 0 };
 	int arrSize = sizeof(arr) / sizeof(arr[0]);
-	int max = 0, min = 0, total = 0;
+	int max = 0x80000000, min = 0x7FFFFFFF, total = 0;
 
 	for (int i = 0; i < arrSize; i++) {
 		printf("정수를 입력하세요 : ");
 		scanf_s("%d", &arr[i]);
 		if (arr[i] >= max) {
 			max = arr[i];
-		}
-		if (arr[i] <= max) {
+		} else if (arr[i] < min) {
 			min = arr[i];
 		}
 		total += arr[i];
