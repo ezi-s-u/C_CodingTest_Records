37aa5b7f6cca0f4c86b8d922bc65f403c0e93401
leejisu <ljsgoo0617@gmail.com>
2023-03-16 00:10:01 +0900

    화씨 온도를 섭씨 온도로 반환하는 FahToCel() 함수 완성

--- C_CodingTest/09-1-2.c
+++ C_CodingTest/09-1-2.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 
-int CelToFah(int c) {
+double CelToFah(int c) {
 	return 1.8 * c + 32;
 }
 
+double FahToCel(int f) {
+	return (f - 32) / 1.8;
+}
+
 int main() {
-	printf("화씨 온도 : %d",CelToFah(5));
+	printf("화씨 온도로 변환한 값 : %.1f\n",CelToFah(5));
+	printf("섭씨 온도로 변환한 값 : %.1f\n", FahToCel(10));
 	return 0;
 }
