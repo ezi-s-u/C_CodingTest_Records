879f529f39f5a6ce260ab15733e53150ae6e7d61
leejisu <ljsgoo0617@gmail.com>
2023-03-15 23:55:33 +0900

    max함수 정의

--- C_CodingTest/09-1-1.c
+++ C_CodingTest/09-1-1.c
@@ -6,3 +6,17 @@ int main() {
 
 	return 0;
 }
+
+max(int a, int b, int c) {
+	int res = 0;
+	if (a > b && a > c) {
+		res = a;
+	}
+	else if (b > a && b > c) {
+		res = b;
+	}
+	else {
+		res = c;
+	}
+	return res;
+}
