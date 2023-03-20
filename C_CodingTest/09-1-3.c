c03f49cfd653c6dcde4180d9f0335e5cd2f9da7c
leejisu <ljsgoo0617@gmail.com>
2023-03-20 23:43:22 +0900

    계산 오류

--- C_CodingTest/09-1-3.c
+++ C_CodingTest/09-1-3.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
 int main(void) {
-	int num, fi = 0;
+	int num, fi = 0, fi2 = 1;
 	printf("몇 개의 피보나치 수열을 출력하시겠습니까\n");
-	scanf("%d", &num);
-
+	scanf_s("%d", &num);
+	printf("\n0 1");
+	for (int i = 0; i < num; i++) {
+		fi += fi2;
+		fi2 = fi;
+		printf("%d ", fi);
+	}
 	return 0;
 }
