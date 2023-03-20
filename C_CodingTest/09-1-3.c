a2daf64830399b644aaef101690f33aa2b05786a
leejisu <ljsgoo0617@gmail.com>
2023-03-21 00:12:25 +0900

    계산 오류 해결, 출력 오류

--- C_CodingTest/09-1-3.c
+++ C_CodingTest/09-1-3.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main(void) {
-	int num, fi = 0, fi2 = 1;
+	int num, f1 = 0, f2 = 1, f3 = 0;
 	printf("몇 개의 피보나치 수열을 출력하시겠습니까\n");
 	scanf_s("%d", &num);
-	printf("\n0 1");
+	printf("\n0 1 ");
 	for (int i = 0; i < num; i++) {
-		fi += fi2;
-		fi2 = fi;
-		printf("%d ", fi);
+		f3 = f1 + f2;
+		printf("%d ", f3);
+		f1 = f2;
+		f2 = f3;
 	}
 	return 0;
 }
