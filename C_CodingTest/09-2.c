8912c32838ff804cf524bdcfbfd56d5c8e4720a0
leejisu <ljsgoo0617@gmail.com>
2023-03-26 22:54:48 +0900

    static 변수로 대체 (09-2 완성)

--- C_CodingTest/09-2.c
+++ C_CodingTest/09-2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int total = 0;
 
 int AddToTotal(int num) {
+	static int total = 0;
 	total += num;
 	return total;
 }
@@ -9,7 +9,7 @@ int main(void) {
 	int num, i;
 	for (i = 0; i < 3; i++) {
 		printf("입력%d: ", i + 1);
-		scanf("%d", &num);
+		scanf_s("%d", &num);
 		printf("누적: %d \n", AddToTotal(num));
 	}
 	return 0;
